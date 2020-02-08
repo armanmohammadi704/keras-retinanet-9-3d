6e24bfab59a73995d9d094b1a92fa989d2a697a6
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-02-09 02:51:48 +0330

    Update resnet.py

--- keras_retinanet/models/resnet.py
+++ keras_retinanet/models/resnet.py
@@ -90,7 +90,7 @@ def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=Non
     # choose default input
     if inputs is None:
         if keras.backend.image_data_format() == 'channels_first':
-            inputs = keras.layers.Input(shape=(9, None, None))
+            inputs = keras.layers.Input(shape=(9, None, None,1))
         else:
             inputs = keras.layers.Input(shape=(None, None, 9,1))
 
